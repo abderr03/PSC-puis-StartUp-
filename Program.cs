4819831225e00ef2d4e09548c22f5c17d9e6b02b
FAdrien <38010723+FAdrien@users.noreply.github.com>
2024-03-05 21:37:38 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+class Test {
+  static void run_cmd(string cmd, string args)
+  {
+      ProcessStartInfo start = new ProcessStartInfo();
+      start.FileName = "python3";
+      start.Arguments = string.Format("{0} {1}", cmd, args);
+      start.UseShellExecute = false;
+      start.RedirectStandardOutput = true;
+      using(Process process = Process.Start(start))
+      {
+          using(StreamReader reader = process.StandardOutput)
+          {
+              string result = reader.ReadToEnd();
+              Console.Write(result);
+          }
+      }
+  }
+
+  static void Main(string[] args) {
+	run_cmd("test.py", "\"bonjour je suis présent !\"");
+  }
+}
